3820a85065a8f9628a8ee26ad4d96824e3f7c98c
Vivian <makinivivian@gmail.com>
2025-05-25 11:16:28 +0300

    Fix 7-multi_c_clean.js to use a loop as required

--- 7-multi_c_clean.js
+++ 7-multi_c_clean.js
@@ -1,11 +1,9 @@
 const x = parseInt(process.argv[2]);
 
-if (isNaN(x) || x <= 0) {
+if (isNaN(x)) {
   console.log("Missing number of occurrences");
 } else {
-  let output = "";
   for (let i = 0; i < x; i++) {
-    output += "C is fun\n";
+    console.log("C is fun");
   }
-  console.log(output.trim());
 }
